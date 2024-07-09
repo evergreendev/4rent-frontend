069ba64b52819d9ad2c70a95ffad009c3bd4931b
Joe simpson <joe@egmrc.com>
2024-07-09 13:20:37 -0600

    Update content block

--- app/BlockRenderer/blocks/ContentBlock.tsx
+++ app/BlockRenderer/blocks/ContentBlock.tsx
@@ -6,7 +6,7 @@ const ContentBlock = ({block}:{block:any}) => {
     return <div className="
     p-4
     prose
-    prose-headings:text-red-600
+    prose-headings:text-red-700
     prose-headings:text-center
     md:prose-xl
     font-opensans
