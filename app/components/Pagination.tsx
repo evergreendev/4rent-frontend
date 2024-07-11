f8cdb725cf188de78392335e29e958ff36a2c9f0
Joe simpson <joe@egmrc.com>
2024-07-11 10:37:58 -0600

    Improve Pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -2,12 +2,13 @@
 import {usePathname, useRouter, useSearchParams} from 'next/navigation';
 const Pagination = ({totalPages}:{totalPages:number}) => {
     const searchParams = useSearchParams();
+    const currPage = searchParams.get("page") || "1";
     const pageArr: any[] = [];
     const pathname = usePathname();
     const { replace } = useRouter();
 
     for (let i = 0; i < totalPages; i++) {
-        pageArr.push(i+1);
+        pageArr.push((i+1).toString());
     }
     function handleClick(page:number){
         const params = new URLSearchParams(searchParams);
@@ -20,7 +21,10 @@ const Pagination = ({totalPages}:{totalPages:number}) => {
     if(pageArr.length <= 1) return <></>;
 
     return pageArr.map((page:any) => {
-        return <button key={page} onClick={()=> handleClick(page)} className="bg-slate-500 text-white p-2">{page}</button>
+        return <button key={page} onClick={()=> handleClick(page)}
+                       className={`bg-slate-500 text-white p-2
+                       ${currPage === page ? "bg-slate-800": ""}
+                       `}>{page}</button>
     })
 }
 
