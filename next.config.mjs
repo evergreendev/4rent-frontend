3f310a17b6d1836d64808d1ad1a63fce0ad0b6e8
Joe simpson <joe@egmrc.com>
2024-07-19 14:20:37 -0600

    Add production remote image host

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
+    images: process.env.NODE_ENV === "production" ? {
+        remotePatterns: [
+            {
+                hostname: process.env.NEXT_PUBLIC_REMOTE_IMAGE_URL
+            }
+        ]
+    } : {
         remotePatterns: [
             {
                 protocol: 'http',
