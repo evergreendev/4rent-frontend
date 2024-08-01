9c5dd72b13e3cc58c23f6e7296664ae8177792b3
Joe simpson <joe@egmrc.com>
2024-08-01 15:04:15 -0600

    Improve pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,16 +1,18 @@
 "use client"
 import {usePathname, useRouter, useSearchParams} from 'next/navigation';
-const Pagination = ({totalPages}:{totalPages:number}) => {
+
+const Pagination = ({totalPages}: { totalPages: number }) => {
     const searchParams = useSearchParams();
     const currPage = searchParams.get("page") || "1";
     const pageArr: any[] = [];
     const pathname = usePathname();
-    const { replace } = useRouter();
+    const {replace} = useRouter();
 
     for (let i = 0; i < totalPages; i++) {
-        pageArr.push((i+1).toString());
+        pageArr.push((i + 1).toString());
     }
-    function handleClick(page:number){
+
+    function handleClick(page: number) {
         const params = new URLSearchParams(searchParams);
 
         params.set('page', page.toString());
@@ -18,14 +20,33 @@ const Pagination = ({totalPages}:{totalPages:number}) => {
         replace(`${pathname}?${params.toString()}`)
     }
 
-    if(pageArr.length <= 1) return <></>;
+    if (pageArr.length <= 1) return <></>;
 
-    return pageArr.map((page:any) => {
-        return <button key={page} onClick={()=> handleClick(page)}
+    const buttonArr = pageArr.map((page: any) => {
+        return <button key={page} onClick={() => handleClick(page)}
                        className={`bg-slate-500 text-white p-2
-                       ${currPage === page ? "bg-slate-800": ""}
+                       ${currPage === page ? "bg-slate-800" : ""}
                        `}>{page}</button>
     })
+
+    return <div>
+        <button className="pr-2" onClick={() => handleClick(1)}>{
+            currPage === "1" ? <span className="font-bold">First</span> : "First"
+        }</button>
+        {
+            currPage !== "1" &&
+            <button className="px-1" onClick={() => handleClick(Math.max((parseInt(currPage) - 1), 1))}>{"<"}</button>
+        }
+        {buttonArr}
+        {
+            parseInt(currPage) !== totalPages &&
+            <button className="px-1"
+                    onClick={() => handleClick(Math.max((parseInt(currPage) + 1), totalPages))}>{">"}</button>
+        }
+        <button className="pl-2" onClick={() => handleClick(totalPages)}>{
+            parseInt(currPage) === totalPages  ? <span className="font-bold">Last</span> : "Last"
+        }</button>
+    </div>;
 }
 
 export default Pagination;
